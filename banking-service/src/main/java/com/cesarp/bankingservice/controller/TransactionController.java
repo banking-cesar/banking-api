41d20121192b85f6229a4b3181298853c5da5e14
CesarArm <cesarpinillos18@gmail.com>
2024-05-05 23:05:48 -0500

    feat: implementacion de repositoru, services, controller

--- banking-service/src/main/java/com/cesarp/bankingservice/controller/TransactionController.java
+++ banking-service/src/main/java/com/cesarp/bankingservice/controller/TransactionController.java
@@ -1,4 +1,42 @@
 package com.cesarp.bankingservice.controller;
 
+import com.cesarp.bankingservice.model.dto.TransactionReportDTO;
+import com.cesarp.bankingservice.model.dto.TransactionRequestDTO;
+import com.cesarp.bankingservice.model.dto.TransactionResponseDTO;
+import com.cesarp.bankingservice.service.TransactionService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/transactions")
+@AllArgsConstructor
 public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    @GetMapping("/accounts/{accountNumber}")
+    public ResponseEntity<List<TransactionResponseDTO>> getTransactionsByAccountNumber(@PathVariable String accountNumber) {
+        List<TransactionResponseDTO> transactions = transactionService.getTransactionsByAccountNumber(accountNumber);
+        return new ResponseEntity<>(transactions, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<TransactionResponseDTO> createTransaction(@Validated @RequestBody TransactionRequestDTO transactionDTO) {
+        TransactionResponseDTO createdTransaction = transactionService.createTransaction(transactionDTO);
+        return new ResponseEntity<>(createdTransaction, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/report")
+    public ResponseEntity<List<TransactionReportDTO>> generateTransactionReport(
+            @RequestParam("startDate") String startDateStr,
+            @RequestParam("endDate") String endDateStr,
+            @RequestParam("accountNumber") String accountNumber) {
+        List<TransactionReportDTO> reportDTOs = transactionService.generateTransactionReport(startDateStr, endDateStr, accountNumber);
+        return new ResponseEntity<>(reportDTOs, HttpStatus.OK);
+    }
 }
--- banking-service/src/main/java/com/cesarp/bankingservice/service/TransactionService.java
+++ banking-service/src/main/java/com/cesarp/bankingservice/service/TransactionService.java
@@ -1,4 +1,77 @@
 package com.cesarp.bankingservice.service;
 
+import com.cesarp.bankingservice.exception.BadRequestException;
+import com.cesarp.bankingservice.exception.ResourceNotFoundException;
+import com.cesarp.bankingservice.mapper.TransactionMapper;
+import com.cesarp.bankingservice.model.dto.TransactionReportDTO;
+import com.cesarp.bankingservice.model.dto.TransactionRequestDTO;
+import com.cesarp.bankingservice.model.dto.TransactionResponseDTO;
+import com.cesarp.bankingservice.model.entity.Account;
+import com.cesarp.bankingservice.model.entity.Transaction;
+import com.cesarp.bankingservice.repository.AccountRepository;
+import com.cesarp.bankingservice.repository.TransactionRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+@AllArgsConstructor
 public class TransactionService {
+    private final TransactionRepository transactionRepository;
+    private final AccountRepository accountRepository;
+    private final TransactionMapper transactionMapper;
+
+    @Transactional(readOnly = true)
+    public List<TransactionResponseDTO> getTransactionsByAccountNumber(String accountNumber) {
+        List<Transaction> transactions = transactionRepository.findBySourceOrTargetAccountNumber(accountNumber);
+        return transactionMapper.convertToListDTO(transactions);
+    }
+
+    @Transactional
+    public TransactionResponseDTO createTransaction(TransactionRequestDTO transactionDTO) {
+        Account sourceAccount = accountRepository.findByAccountNumber(transactionDTO.getSourceAccountNumber())
+                .orElseThrow(()-> new ResourceNotFoundException("La cuenta de origen no existe"));
+        Account targetAccount = accountRepository.findByAccountNumber(transactionDTO.getTargetAccountNumber())
+                .orElseThrow(()-> new ResourceNotFoundException("La cuenta de origen no existe"));
+
+        BigDecimal amount = transactionDTO.getAmount();
+        BigDecimal sourceAccountBalance = sourceAccount.getBalance();
+        if (sourceAccountBalance.compareTo(amount) < 0){
+            throw new BadRequestException("Saldo insuficiente en la cuenta origen");
+        }
+
+        Transaction transaction = transactionMapper.convertToEntity(transactionDTO);
+        transaction.setTransactionDate(LocalDate.now());
+        transaction.setSourceAccount(sourceAccount);
+        transaction.setTargetAccount(targetAccount);
+        transaction = transactionRepository.save(transaction);
+
+        BigDecimal newSourceAccountBalance = sourceAccountBalance.subtract(amount);
+        BigDecimal targetAccountBalance = targetAccount.getBalance().add(amount);
+
+        sourceAccount.setBalance(newSourceAccountBalance);
+        targetAccount.setBalance(targetAccountBalance);
+
+        accountRepository.save(sourceAccount);
+        accountRepository.save(targetAccount);
+
+        return transactionMapper.convertToDTO(transaction);
+    }
+
+    @Transactional(readOnly = true)
+    public List<TransactionReportDTO> generateTransactionReport(String startDateStr,
+                                                                String endDateStr,
+                                                                String accountNumber) {
+        LocalDate startDate = LocalDate.parse(startDateStr);
+        LocalDate endDate = LocalDate.parse(endDateStr);
+
+        List<Object[]> transactionCounts = transactionRepository.getTransactionCountByDateRangeAndAccountNumber(startDate, endDate, accountNumber);
+        return transactionCounts.stream()
+                .map(transactionMapper::convertTransactionReportDTO)
+                .toList();
+    }
 }
